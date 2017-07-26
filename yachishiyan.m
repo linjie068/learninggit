71bb22840197f9a5437270313a6b2e6cd0fb4db4
linjie068 <406411775@qq.com>
2017-07-26 20:46:49 +0800

    add new file

---
+++ yachishiyan.m
@@ -0,0 +1,9 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%牙齿实验
+clc;clear all;close all
+image=dicomread('C:\Users\caoqj\Desktop\科研\9\CT1.2.840.113619.2.278.3.3633713111.455.1442835087.895.80.dcm');
+image=double(image)cdjfh
+jkfdhgvjkg 
+vfjgh 
+vjfjhgjfg
+bdbvfhdf 
+ghfdghk 
